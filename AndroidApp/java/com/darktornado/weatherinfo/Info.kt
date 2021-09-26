7f9fa601cb23b9eb113b080236c012ab8c793c7c
Dark Tornado <dt3141592@naver.com>
2021-09-27 01:13:51 +0900

    Create Info.kt

---
+++ AndroidApp/java/com/darktornado/weatherinfo/Info.kt
@@ -0,0 +1,5 @@
+package com.darktornado.weatherinfo
+
+data class Info (val time: String, val status: String,
+                 val temp: String, val hum: String, val rain: String,
+                 val windDir: String, val windSpeed: String)
